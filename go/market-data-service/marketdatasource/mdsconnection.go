051c75e1de4d35ca315229e50d2f14b81b204a96
matthewpendrey <matthew.pendrey@gmail.com>
2020-04-02 15:58:21 +0100

    3rd test pre-commit format hook

--- go/market-data-service/marketdatasource/mdsconnection.go
+++ go/market-data-service/marketdatasource/mdsconnection.go
@@ -61,7 +61,7 @@ func (s *MdsConnection) AddConnection(subscriberId string, out chan<- *model.Clo
 	if conn, ok := s.partyIdToConnection[subscriberId]; ok {
 		log.Printf("connection for client %v already exists, closing existing connection.", subscriberId)
 		conn.Close()
-		log.Print("connection closed ", subscriberId)
+		log.Print("connection closed: ", subscriberId)
 	}
 
 	cc := marketdata.NewConflatedQuoteConnection(subscriberId, out, s.quoteDistributor, s.maxSubscriptions)
