4f55f37b224754f58cf96702db202fd24ad9096e
matthewpendrey <matthew.pendrey@gmail.com>
2020-06-08 14:24:09 +0100

    place holder authentication and authorisation service

--- go/authorization-service/go.mod
+++ go/authorization-service/go.mod
@@ -1,11 +1,19 @@
 module github.com/ettech/open-trading-platform/go/authorization-service
 
+go 1.13
+
 require (
 	github.com/envoyproxy/go-control-plane v0.9.5
+	github.com/ettec/open-trading-platform/go/common v0.0.0
+	github.com/ettec/open-trading-platform/go/model v0.0.0
 	github.com/gogo/googleapis v1.4.0
-	github.com/golang/protobuf v1.3.2
+	github.com/golang/protobuf v1.4.0
+	github.com/google/uuid v1.1.1
+	github.com/lib/pq v1.2.0
 	google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55
 	google.golang.org/grpc v1.25.1
 )
 
-go 1.13
+replace github.com/ettec/open-trading-platform/go/common v0.0.0 => ../common
+
+replace github.com/ettec/open-trading-platform/go/model v0.0.0 => ../model
--- go/authorization-service/service.go
+++ go/authorization-service/service.go
@@ -2,104 +2,117 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"errors"
 	"github.com/envoyproxy/go-control-plane/envoy/api/v2/core"
 	auth "github.com/envoyproxy/go-control-plane/envoy/service/auth/v2"
 	envoytype "github.com/envoyproxy/go-control-plane/envoy/type"
+	"github.com/ettec/open-trading-platform/go/common/bootstrap"
 	"github.com/ettech/open-trading-platform/go/authorization-service/api/loginservice"
 	rpc "github.com/gogo/googleapis/google/rpc"
+	"github.com/google/uuid"
 	status "google.golang.org/genproto/googleapis/rpc/status"
 	"google.golang.org/grpc"
 	"log"
 	"net"
+	"strings"
+	_ "github.com/lib/pq"
 )
 
-type LoginService struct{}
 
-func (l LoginService) Login(ctx context.Context, params *loginservice.LoginParams) (*loginservice.Token, error) {
-	if params.User == "bert" && params.Password == "poop" {
+func (a AuthService) Login(ctx context.Context, params *loginservice.LoginParams) (*loginservice.Token, error) {
+
+	if user, ok := a.users[params.User]; ok  {
 		return &loginservice.Token{
-			Token: "bob",
-			Desk:  "Delta1",
-		}, nil
-	}
-
-	return nil, errors.New("invalid username/password")
-}
-
-type LoginServer struct{}
-
-func (l LoginServer) Login(ctx context.Context, params *loginservice.LoginParams) (*loginservice.Token, error) {
-
-	if params.User == "bert" && params.Password == "password" {
-		return &loginservice.Token{
-			Token:                "bob",
-			Desk:                 "Delta1",
-
+			Token: user.token,
+			Desk:  user.desk,
 		}, nil
 	}
 
 	return nil, errors.New("user not found")
 }
 
-// empty struct because this isn't a fancy example
-type AuthorizationServer struct{}
+type AuthService struct{
+	users map[string]user
+}
 
 // inject a header that can be used for future rate limiting
-func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
+func (a *AuthService) Check(ctx context.Context, req *auth.CheckRequest) (*auth.CheckResponse, error) {
 
 	path, ok := req.Attributes.Request.Http.Headers[":path"]
-	if ok && path == "/loginservice.LoginService/Login" {
-		log.Print("allowing it:", req)
-		return &auth.CheckResponse{
-			Status: &status.Status{
-				Code: int32(rpc.OK),
-			},
-			HttpResponse: &auth.CheckResponse_OkResponse{
-				OkResponse: &auth.OkHttpResponse{
-					Headers: []*envoy_api_v2_core.HeaderValueOption{
-						{
-							Header: &envoy_api_v2_core.HeaderValue{
-								// here is where set trading perms
-								Key:   "somestuff",
-								Value: "ratatouey",
-							},
-						},
-					},
-				},
-			},
-		}, nil
+	if ok && strings.HasPrefix(path, "/loginservice.LoginService") {
+		return newOkResponse(), nil
 	}
 
-	log.Print("checking it:", req)
 
 	authHeader, ok := req.Attributes.Request.Http.Headers["auth-token"]
+	if !ok {
 
-	if ok && authHeader == "bob" {
+	}
 
-		// valid tokens have exactly 3 characters. #secure.
-		// Normally this is where you'd go check with the system that knows if it's a valid token.
-		return &auth.CheckResponse{
-			Status: &status.Status{
-				Code: int32(rpc.OK),
-			},
-			HttpResponse: &auth.CheckResponse_OkResponse{
-				OkResponse: &auth.OkHttpResponse{
-					Headers: []*envoy_api_v2_core.HeaderValueOption{
-						{
-							Header: &envoy_api_v2_core.HeaderValue{
-								// here is where set trading perms
-								Key:   "x-ext-auth-ratelimit",
-								Value: "ratatouey",
-							},
+	username, ok := req.Attributes.Request.Http.Headers["user-name"]
+	if !ok {
+		return newUnauthenticatedResponse("No user-name found on request"), nil
+	}
+
+	user, ok := a.users[username]
+	if !ok {
+		return newUnauthenticatedResponse("user not found"), nil
+	}
+
+	if user.token != authHeader {
+		return newUnauthenticatedResponse("invalid token"), nil
+	}
+
+	// Authorisation
+	if ok && strings.HasPrefix(path, "/executionvenue.ExecutionVenue") {
+		if strings.Contains(user.permissionFlags, "T") {
+			return newOkResponse(), nil
+		} else {
+			return newPermissionDeniedResponse("trading permissions required"), nil
+		}
+	}
+
+	return newOkResponse(), nil
+}
+
+func newOkResponse() *auth.CheckResponse {
+	return &auth.CheckResponse{
+		Status: &status.Status{
+			Code: int32(rpc.OK),
+		},
+		HttpResponse: &auth.CheckResponse_OkResponse{
+			OkResponse: &auth.OkHttpResponse{
+				Headers: []*envoy_api_v2_core.HeaderValueOption{
+					{
+						Header: &envoy_api_v2_core.HeaderValue{
+							Key:   "authorised",
+							Value: "true",
 						},
 					},
 				},
 			},
-		}, nil
-
+		},
 	}
+}
 
+func newPermissionDeniedResponse(message string) *auth.CheckResponse {
+	return &auth.CheckResponse{
+		Status: &status.Status{
+			Code: int32(rpc.PERMISSION_DENIED),
+		},
+		HttpResponse: &auth.CheckResponse_DeniedResponse{
+			DeniedResponse: &auth.DeniedHttpResponse{
+				Status: &envoytype.HttpStatus{
+					Code: envoytype.StatusCode_Unauthorized,
+				},
+				Body: message,
+			},
+		},
+	}
+}
+
+func newUnauthenticatedResponse(message string) *auth.CheckResponse {
 	return &auth.CheckResponse{
 		Status: &status.Status{
 			Code: int32(rpc.UNAUTHENTICATED),
@@ -109,14 +122,59 @@ func (a *AuthorizationServer) Check(ctx context.Context, req *auth.CheckRequest)
 				Status: &envoytype.HttpStatus{
 					Code: envoytype.StatusCode_Unauthorized,
 				},
-				Body: "No authorisation token found",
+				Body: message,
 			},
 		},
-	}, nil
+	}
+}
+
+const (
+
+	DatabaseConnectionString = "DB_CONN_STRING"
+	DatabaseDriverName       = "DB_DRIVER_NAME"
+)
+
+type user struct {
+	id string
+	desk string
+	permissionFlags string
+	token string
 }
 
 func main() {
 
+	dbString := bootstrap.GetEnvVar(DatabaseConnectionString)
+	dbDriverName := bootstrap.GetEnvVar(DatabaseDriverName)
+
+
+
+	db, err := sql.Open(dbDriverName, dbString)
+	defer db.Close()
+	if err != nil {
+		log.Panicf("failed to open database connection: %w", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("could not establish a connection with the database: ", err)
+	}
+
+	r, err := db.Query("SELECT id, desk, permissionflags FROM users.users")
+	if err != nil {
+		log.Panicf("failed to get users from database")
+	}
+
+	users := map[string]user{}
+	for r.Next() {
+		u := user{}
+		r.Scan(&u.id, &u.desk, &u.permissionFlags)
+		u.token = uuid.New().String()
+		users[u.id] = u
+	}
+
+	log.Printf("loaded %v users", len(users))
+
+	authServer := &AuthService{users: users}
 
 	go func() {
 		loginPort := "50551"
@@ -127,16 +185,13 @@ func main() {
 		log.Printf("listening on %s", lis.Addr())
 
 		grpcServer := grpc.NewServer()
-		loginServer := &LoginServer{}
-		loginservice.RegisterLoginServiceServer(grpcServer, loginServer)
+		loginservice.RegisterLoginServiceServer(grpcServer, authServer)
 
 		log.Print("starting login server of port:", loginPort)
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("Failed to start server: %v", err)
 		}
 	}()
-
-
 
 	authPort := "4000"
 	lis, err := net.Listen("tcp", ":"+authPort)
@@ -146,7 +201,7 @@ func main() {
 	log.Printf("listening on %s", lis.Addr())
 
 	grpcServer := grpc.NewServer()
-	authServer := &AuthorizationServer{}
+
 	auth.RegisterAuthorizationServer(grpcServer, authServer)
 
 	log.Print("starting authorization server of port:", authPort)
