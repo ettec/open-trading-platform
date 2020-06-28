d1424a01b5ae353e6828e5c9c5d3b02e1aece3de
matthewpendrey <matthew.pendrey@gmail.com>
2020-06-28 18:33:49 +0100

    deploy test

--- go/execution-venues/fix-sim-execution-venue/service.go
+++ go/execution-venues/fix-sim-execution-venue/service.go
@@ -70,7 +70,7 @@ func main() {
 
 	port := "50551"
 	fmt.Println("Starting Execution Venue Service on port:" + port)
-	fmt.Println("V1")
+	fmt.Println("V2")
 	lis, err := net.Listen("tcp", "0.0.0.0:"+port)
 
 	if err != nil {
