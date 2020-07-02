066ecf9aad2f27b6ceae6d8b0bc52b96a1515adc
matthewpendrey <matthew.pendrey@gmail.com>
2020-07-02 14:30:06 +0100

    opentp k8s deployment yaml

--- kube/markets/createmarketobjects.sh
+++
@@ -1,8 +0,0 @@
-kubectl apply -f sim-deployment.yaml 
-kubectl apply -f sim-service.yaml 
-kubectl get pods
-kubectl get service
-kubectl apply -f order_gateway.yaml 
-kubectl apply -f order-gateway-service.yaml 
-kubectl apply -f mdgateway.yaml 
-kubectl apply -f md-gateway-service.yaml
