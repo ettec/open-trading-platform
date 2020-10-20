f4b3b2494b8e4d5de5938acf0334086787748725
matthewpendrey <matthew.pendrey@gmail.com>
2020-10-20 15:45:30 +0100

    releaseAll script fix

--- go/scripts/releaseAllLocal.sh
+++ go/scripts/releaseAllLocal.sh
@@ -1,5 +1,4 @@
-DIRECTORY=$(cd `dirname $0` && pwd)
-cd $DIRECTORY/..
+DIRECTORY=$(cd `dirname $0` && pwd)/..
 echo working dir `pwd`	
 
 RELEASEDFILE=$DIRECTORY/lastreleased.txt
@@ -7,12 +6,13 @@ RELEASEDFILE=$DIRECTORY/lastreleased.txt
 
 echo -n "" > $RELEASEDFILE 
 
+cd $DIRECTORY
 find . -type f -name '*go.mod*' | sed -r 's|/[^/]+$||' |sort |uniq | while read line; do
 cd $line
 
 if ls *.go 1> /dev/null 2>&1;
 then
-       $DIRECTORY/releaseLocal.sh $RELEASEDFILE 
+       $DIRECTORY/scripts/releaseLocal.sh $RELEASEDFILE 
 fi
 cd $DIRECTORY
    
